62a1645d4c8c03880dbfeea3870c5964f3e884c2
Bryan Garro Eduarte <bryangarroeduarte@hotmail.com>
2020-02-28 11:08:42 -0600

    business logic

---
+++ backend/src/BusinessLogic/Profesor.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BusinessLogic;
+
+/**
+ *
+ * @author Josue
+ */
+public class Profesor {
+    private String cedula;
+    private String nombre;
+    private String telefono;
+    private String email;
+    
+    public Profesor(){
+        this.cedula = new String();
+        this.nombre = new String();
+        this.cedula = new String();
+        this.email = new String();
+    }
+    public Profesor(String cedula, String nombre, String telefono, String email) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Profesor{" + "cedula=" + cedula + ", nombre=" + nombre + ", telefono=" + telefono + ", email=" + email + '}';
+    }
+    
+}
