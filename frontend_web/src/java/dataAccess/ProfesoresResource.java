a40d90dcbd13a8eff189b95cb24d1ac7e1182e55
Josue <josue.cespedes19@gmail.com>
2020-03-04 21:01:13 -0600

    Testing

--- frontend_web/src/java/dataAccess/ProfesoresResource.java
+++ frontend_web/src/java/dataAccess/ProfesoresResource.java
@@ -15,9 +15,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
-import static javax.ws.rs.HttpMethod.DELETE;
+
 import javax.ws.rs.POST;
-import static javax.ws.rs.HttpMethod.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.DELETE;
@@ -84,6 +83,7 @@ public class ProfesoresResource {
     public int eliminar(@PathParam("id") int id){
         Model dm1 = Model.instance();
         dm = new Control(dm1);
+        return 0;
         
     }
 }
